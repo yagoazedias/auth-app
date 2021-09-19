e126175731c60996820c78a7de258a1078660fe1
Yago Azedias <yagoazedias@gmail.com>
2021-09-19 20:58:09 -0300

    Adding tests for 'highfrequency-small-interval' violation

--- tests/business/test_transaction.py
+++ tests/business/test_transaction.py
@@ -6,6 +6,7 @@ from lib.business.transaction import TransactionAuthorizer
 
 class TestTransactionAuthorizer(unittest.TestCase):
 
+    @patch('lib.repository.statement.Statement.set_operation')
     @patch('lib.repository.statement.Statement.set_account_limit')
     @patch('lib.repository.statement.Statement.get_card_balance')
     @patch('lib.repository.statement.Statement.is_account_created')
@@ -13,7 +14,9 @@ class TestTransactionAuthorizer(unittest.TestCase):
     def test_review_operation_without_violations(self, mock_statement_get_account,
                                                  mock_statement_is_account_created,
                                                  mock_statement_get_card_balance,
-                                                 mock_statement_set_account_limit):
+                                                 mock_statement_set_account_limit,
+                                                 mock_statement_set_operation):
+        mock_statement_set_operation.return_value = None
         mock_statement_set_account_limit.return_value = None
         mock_statement_get_card_balance.return_value = 225
         mock_statement_is_account_created.return_value = True
@@ -24,6 +27,7 @@ class TestTransactionAuthorizer(unittest.TestCase):
         expected = {"account": {"active-card": True, "available-limit": 225}, "violations": []}
         self.assertEqual(expected, result)
 
+    @patch('lib.repository.statement.Statement.set_operation')
     @patch('lib.repository.statement.Statement.set_account_limit')
     @patch('lib.repository.statement.Statement.get_card_balance')
     @patch('lib.repository.statement.Statement.is_account_created')
@@ -31,7 +35,9 @@ class TestTransactionAuthorizer(unittest.TestCase):
     def test_review_operation_with_account_not_initialized_violation(self, mock_statement_get_account,
                                                                      mock_statement_is_account_created,
                                                                      mock_statement_get_card_balance,
-                                                                     mock_statement_set_account_limit):
+                                                                     mock_statement_set_account_limit,
+                                                                     mock_statement_set_operation):
+        mock_statement_set_operation.return_value = None
         mock_statement_set_account_limit.return_value = None
         mock_statement_get_card_balance.return_value = 225
         mock_statement_is_account_created.return_value = False
@@ -42,6 +48,7 @@ class TestTransactionAuthorizer(unittest.TestCase):
         expected = {"account": {}, "violations": ["account-not-initialized"]}
         self.assertEqual(expected, result)
 
+    @patch('lib.repository.statement.Statement.set_operation')
     @patch('lib.repository.statement.Statement.set_account_limit')
     @patch('lib.repository.statement.Statement.get_card_balance')
     @patch('lib.repository.statement.Statement.is_account_created')
@@ -49,8 +56,9 @@ class TestTransactionAuthorizer(unittest.TestCase):
     def test_review_operation_with_apply_card_not_active(self, mock_statement_get_account,
                                                          mock_statement_is_account_created,
                                                          mock_statement_get_card_balance,
-                                                         mock_statement_set_account_limit):
-
+                                                         mock_statement_set_account_limit,
+                                                         mock_statement_set_operation):
+        mock_statement_set_operation.return_value = None
         mock_statement_set_account_limit.return_value = None
         mock_statement_get_card_balance.return_value = 200
         mock_statement_is_account_created.return_value = True
@@ -59,4 +67,37 @@ class TestTransactionAuthorizer(unittest.TestCase):
         operation = {"transaction": {"merchant": "Uber Eats", "amount": 25, "time": "2020-12-01T11:07:00.000Z"}}
         result = TransactionAuthorizer().review_operation(operation)
         expected = {"account": {"active-card": False, "available-limit": 225}, "violations": ["card-not-active"]}
+        self.assertEqual(expected, result)
+
+    @patch('lib.repository.statement.Statement.set_operation')
+    @patch('lib.repository.statement.Statement.set_account_limit')
+    @patch('lib.repository.statement.Statement.get_card_balance')
+    @patch('lib.repository.statement.Statement.is_account_created')
+    @patch('lib.repository.statement.Statement.get_account')
+    @patch('lib.repository.statement.Statement.get_transactions_operations')
+    def test_review_operation_with_highfrequency_small_interval(self,
+                                                                mock_statement_get_transactions_operations,
+                                                                mock_statement_get_account,
+                                                                mock_statement_is_account_created,
+                                                                mock_statement_get_card_balance,
+                                                                mock_statement_set_account_limit,
+                                                                mock_statement_set_operation):
+        mock_statement_set_operation.return_value = None
+        mock_statement_set_account_limit.return_value = None
+        mock_statement_get_card_balance.return_value = 225
+        mock_statement_is_account_created.return_value = True
+        mock_statement_get_account.return_value = {"active-card": True, "available-limit": 225}
+
+        mock_statement_get_transactions_operations.return_value = [
+            {"operation": {
+                "transaction": {"merchant": "Burger King", "amount": 20, "time": "2019-02-13T11:00:00.000Z"}}},
+            {"operation": {"transaction": {"merchant": "Habbib's", "amount": 20, "time": "2019-02-13T11:00:01.000Z"}}},
+            {"operation": {"transaction": {"merchant": "McDonald's", "amount": 20, "time": "2019-02-13T11:01:01.000Z"}}}
+        ]
+
+        operation = {"transaction": {"merchant": "Subway", "amount": 20, "time": "2019-02-13T11:01:31.000Z"}}
+        result = TransactionAuthorizer().review_operation(operation)
+        expected = {"account": {"active-card": True, "available-limit": 225},
+                    "violations": ["highfrequency-small-interval"]}
+
         self.assertEqual(expected, result)
