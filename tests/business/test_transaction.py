e0d6bdbd6372353673a0bfb2252858f87e109db6
Yago Azedias <yagoazedias@gmail.com>
2021-09-20 00:04:53 -0300

    Adding 'test_review_operation_with_insufficient_limit' insufficient_limit test

--- tests/business/test_transaction.py
+++ tests/business/test_transaction.py
@@ -74,6 +74,30 @@ class TestTransactionAuthorizer(unittest.TestCase):
     @patch('lib.repository.statement.Statement.get_card_balance')
     @patch('lib.repository.statement.Statement.is_account_created')
     @patch('lib.repository.statement.Statement.get_account')
+    def test_review_operation_with_insufficient_limit(self,
+                                                      mock_statement_get_account,
+                                                      mock_statement_is_account_created,
+                                                      mock_statement_get_card_balance,
+                                                      mock_statement_set_account_limit,
+                                                      mock_statement_set_operation):
+        mock_statement_set_operation.return_value = None
+        mock_statement_set_account_limit.return_value = None
+        mock_statement_get_card_balance.return_value = 225
+        mock_statement_is_account_created.return_value = True
+        mock_statement_get_account.return_value = {"active-card": True, "available-limit": 225}
+
+        operation = {"transaction": {"merchant": "Subway", "amount": 2000, "time": "2019-02-13T11:01:31.000Z"}}
+        result = TransactionAuthorizer().review_operation(operation)
+        expected = {"account": {"active-card": True, "available-limit": 225},
+                    "violations": ["insufficient-limit"]}
+
+        self.assertEqual(expected, result)
+
+    @patch('lib.repository.statement.Statement.set_operation')
+    @patch('lib.repository.statement.Statement.set_account_limit')
+    @patch('lib.repository.statement.Statement.get_card_balance')
+    @patch('lib.repository.statement.Statement.is_account_created')
+    @patch('lib.repository.statement.Statement.get_account')
     @patch('lib.repository.statement.Statement.get_transactions_operations')
     def test_review_operation_with_highfrequency_small_interval(self,
                                                                 mock_statement_get_transactions_operations,
