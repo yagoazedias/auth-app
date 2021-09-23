4895fa56eec55c37d641fadb4d32c5f9726e690b
Yago Azedias <yagoazedias@gmail.com>
2021-09-22 21:04:59 -0300

    Adding integration tests

--- tests/all.py
+++ tests/all.py
@@ -4,6 +4,7 @@ from tests.business.test_account import TestAccountAuthorizer
 from tests.business.test_transaction import TestTransactionAuthorizer
 from tests.controller.test_interface import TestControllerInterface
 from tests.repository.test_statement import TestStatementInterface
+from tests.integration import TestIntegration
 
 if __name__ == '__main__':
     unittest.main()
