0076970fece015eb14d40ea45872c8566ba54001
yagoazedias <yagoazedias@gmail.com>
2025-02-15 20:59:08 -0300

    Refactor get_specific_authorizer_by_operation to accept authorizers as a parameter for improved flexibility

--- lib/controller/interface.py
+++ lib/controller/interface.py
@@ -16,14 +16,15 @@ class Interface:
     def _convert_input_file_line_to_dict(line):
         return json.loads(line)
 
-    def get_specific_authorizer_by_operation(self, operation):
-        for key, authorizer in self.authorizers.items():
+    @staticmethod
+    def get_specific_authorizer_by_operation(operation, authorizers):
+        for key, authorizer in authorizers.items():
             if key in operation:
                 return authorizer()
 
     def apply_operations_from_file_input(self, file_input):
         operations = pydash.map_(file_input, self._convert_input_file_line_to_dict)
-        outputs = pydash.map_(operations, lambda operation: self.get_specific_authorizer_by_operation(operation)
+        outputs = pydash.map_(operations, lambda operation: self.get_specific_authorizer_by_operation(operation, self.authorizers)
                               .review_operation(operation))
 
         for output in outputs:
